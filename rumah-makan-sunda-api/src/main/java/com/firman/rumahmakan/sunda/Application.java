8dbbeef6e0251ce7b072e1fea155e48c14ea7ced
Firman Hanafi <firman.hanafi@nsiapay.net>
2020-01-09 18:40:02 +0700

    initiate project

---
+++ rumah-makan-sunda-api/src/main/java/com/firman/rumahmakan/sunda/Application.java
@@ -0,0 +1,14 @@
+package com.firman.rumahmakan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(com.firman.rumahmakan.Application.class, args);
+    }
+
+}
